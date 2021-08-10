3fd8534a2d71be80f235c62f872c3d8b60e17a17
devshini <sej0312@gmail.com>
2021-08-10 17:31:40 +0900

    Day1

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,21 @@
+console.log('========= assignments ==========');
+
+// 1. Values and Variables
+
+let country = 'Republic of Korea';
+let continent = 'Asia';
+let population = 51;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// 2. Data Types
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,63 @@
+/* let js = 'amazing';
+
+// - Linking a JavaScript File
+// if (js === 'amazing') alert ('JavaScript is FUN!');
+// 40+8+23-10;
+
+// console.log(40+8+23-10); 주석처리 단축키 command + /
+
+console.log("Shini");
+console.log(23);
+
+let firstName = "Shini"; // camelCase
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let shini_eunjung = "SEJ";
+let $function = 35;
+
+let person = 'shini';
+let PI = 3.1415;
+
+let myFirstJob = 'oL';
+let myCurrentJob = 'Programmer';
+
+let job1 = 'OL';
+let job2 = 'Programmer';
+
+console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Shini');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1987;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+
+
+
+
+
+
+
+
+
