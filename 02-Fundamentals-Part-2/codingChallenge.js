591b82da178b16ec90684df0b49e89a8aa01dd2d
devshini <sej0312@gmail.com>
2021-08-14 21:52:56 +0900

    DAY5

--- 02-Fundamentals-Part-2/codingChallenge.js
+++ 02-Fundamentals-Part-2/codingChallenge.js
@@ -50,7 +50,6 @@ checkWinner(scoreDolphis, scoreKoalas);
 const calcTip = bill => 
 bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-
 const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 console.log(bills, tips);
